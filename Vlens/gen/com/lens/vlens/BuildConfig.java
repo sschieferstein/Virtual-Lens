9961fa014875f900e20d8bd4c201767a538d9aeb
bernlim93 <me@bernlim.com>
2014-10-11 16:27:46 -0500

    Add Vlens new project

---
+++ Vlens/gen/com/lens/vlens/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.lens.vlens;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Vlens/src/com/lens/vlens/Options.java
@@ -0,0 +1,16 @@
+package com.lens.vlens;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Options extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.frag_options, container, false);
+    }
+}
